3c1dd534ec3dba81585b92f12fd5e6cbd446f5fe
João Bertholino <joaobertholino964@gmail.com>
2023-10-23 11:59:48 -0300

    Subindo mais conteúdo

--- JavaScript Moderno/js-modern.js
+++ JavaScript Moderno/js-modern.js
@@ -9,7 +9,7 @@ const testeVar = () => {
   var x = 1;
   if (true) {
     var x = 2;
-    console.log(x); // "var" => Independente do escopo, sempre usara a mesma variavel e não da erro ao criar variavel com o mesmo nome
+    console.log(x); // "var" => Independente do escopo, sempre usara a mesma variável e não da erro ao criar variável com o mesmo nome
   }
   console.log(x);
 };
@@ -20,7 +20,7 @@ const testeLet = () => {
   let x = 1;
   if (true) {
     let x = 2;
-    console.log(x); // "let" => Pertence apenas ao seu proprio escopo, não sendo visiveis em outros escopos
+    console.log(x); // "let" => Pertence apenas ao seu próprio escopo, não sendo visíveis em outros escopos
   }
   console.log(x);
 };
@@ -35,7 +35,7 @@ console.log(soma(3, 5)); // Resume operações como "math", "reduce", "filter" e
 const quadrado = (numero) => numero * numero; // Retorna o valor ao quadrado de um numero
 console.log(quadrado(5));
 
-// Arrow function => Metodo convencional, usando chaves para criar um bloco
+// Arrow function => Método convencional, usando chaves para criar um bloco
 const cumprimento = (nome) => {
   const mensagem = `Olá, ${nome}!`; // Interpolação
   return mensagem; // Quando tem uma função de bloco em arrow function, tem que retornar
@@ -43,24 +43,72 @@ const cumprimento = (nome) => {
 
 console.log(cumprimento("Matheus"));
 
-// Template literals => Recurso que permite inserir variaveis dentro de uma string
+// Template literals => Recurso que permite inserir variáveis dentro de uma string
 const nome = "Maria";
 const idade = 22;
-console.log(`Meu nome é ${nome} e eu tenho ${2 + 8 + 12} anos.`); // Permite, alem de variaveis, expreções numericas e textuais
+console.log(`Meu nome é ${nome} e eu tenho ${2 + 8 + 12} anos.`); // Permite, além de variáveis, expressões numéricas e textuais
 
 // Destructuring => Desestruturação
-// Objeto => Pode renomear uma variavel e selecionar propriedades específicas
+// Objeto => Pode renomear uma variável e selecionar propriedades específicas
 const pessoa = { nome: "João", idade: 40, profissão: "Programador" };
 
-const { nome2, idade2, profissão } = pessoa; // A converção automatica só ocorre quando o nome da chave é igual ao nome da variavel
+const { nome2, idade2, profissão } = pessoa; // A conversão automática só ocorre quando o nome da chave é igual ao nome da variável
 console.log(nome2, idade2, profissão);
 
-const { nome: nome3, idade: idade3 } = pessoa; // Convertendo o nome da chave para o nome da variavel => nomeDaVariavel: nomeDaChave
+const { nome: nome3, idade: idade3 } = pessoa; // Convertendo o nome da chave para o nome da variável => nomeDaVariável: nomeDaChave
 console.log(nome3, idade3);
 
 // Array => Depende da ordem dos elementos
-const numeros = [1, 2, 3, 4];
-const [primeiro, segundo, terceiro] = numeros; // Funciona na ordem dos índices pelo fato do array não ser possivel chamar o seu nome assim como o objeto tem 
+const números = [1, 2, 3, 4];
+const [primeiro, segundo, terceiro] = números; // Funciona na ordem dos índices pelo fato do array não ser possível chamar o seu nome assim como o objeto tem
 console.log(primeiro, segundo, terceiro);
 
-// 3:34:40 - Tempo da aula
+// spread operator => Espalhamento
+const array1 = [1, 2, 3, 4];
+const array2 = [...array1, 4, 5, 6]; // Insere todos os elementos do array1 dentro do array2
+console.log(array2);
+
+const soma2 = (...args) => args.reduce((total, numero) => total + numero, 0); // Uma variável que recebe um valor variável usando o operador "...args" e, em seguida, usa o método "reduce" para somar todos os valores
+console.log(soma2(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+// Default parameters
+const saudação = (nome = "Visitante") => {
+  console.log(`Olá, ${nome}!`);
+};
+
+saudação(); // Caso passe nenhum valor, nome sera igual a "Visitante"
+saudação("Matheus"); // nome = "Matheus"
+
+// Object literals enhancements
+const nomeNovo = "Felipe";
+const idadeNova = 24;
+const pessoaNova1 = { nomeNovo, idadeNova }; // Fluxo inverso do "destructuring", porem, para criar objeto e não desestruturar
+const pessoaNova2 = { nome: nomeNovo, idade: idadeNova }; // Renomeando a chaves
+console.log(pessoaNova1);
+console.log(pessoaNova2);
+
+const saudaçãoObject = {
+  dizerOi() {
+    console.log("Oi!"); // Sem utilizar de orientação a objetos e sem nomear a função, executar uma função por meio de object literal
+  },
+};
+saudaçãoObject.dizerOi();
+
+// Classes e Heranças
+class Pessoa {
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+  falar() {
+    console.log(`Olá pessoal, meu nome é ${this.nome}`);
+  }
+}
+
+const João = new Pessoa("João", 34);
+console.log(João);
+
+João.falar();
+
+// 3:42:51 - Tempo da aula
