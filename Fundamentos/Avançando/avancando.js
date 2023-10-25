1ea152ccaa6b1ba38f477351586d9e4c9e976abb
João Bertholino <joaobertholino964@gmail.com>
2023-10-25 12:02:21 -0300

    Subindo ajustes

--- Fundamentos/Avançando/avancando.js
+++ Fundamentos/Avançando/avancando.js
@@ -1,43 +1,46 @@
 // Manipulação de arrays
 const frutas = ["Maça", "Melancia"];
+console.log(frutas);
 
 frutas.unshift("Uva"); // Adiciona um elemento no inicio do array
-frutas.push("Laranja"); // Adiciona um elemento no final do array
-frutas.shift(); // Remove o primeiro elemento do array
-frutas.pop(); // Remove o ultimo elemento do array
-
 console.log(frutas);
+
+frutas.push("Laranja"); // Adiciona um elemento no final do array
+console.log(frutas);
+
+frutas.shift(); // Remove o primeiro elemento do array
+console.log(frutas);
+
+frutas.pop(); // Remove o ultimo elemento do array
 console.log(frutas);
 
 // Map, Filter, Reduce => Arrow function
 const números = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const numeroPar = números.find((num) => num % 2 === 0); // O método "find" retorna apenas o primeiro elemento do critério
-const númerosPares = números.filter((numP) => numP % 2 === 0); // O método "Filter" retorna todos os elementos de acordo com o critério
 
+const numeroPar = números.find((num) => num % 2 === 0); // O método "find" retorna apenas o primeiro elemento do critério
 console.log(numeroPar);
+
+const númerosPares = números.filter((numP) => numP % 2 === 0); // O método "Filter" retorna todos os elementos do critério
 console.log(númerosPares);
 
 // Manipulação de strings
 const frase = "Olá, mundo!     ";
-const palavras = frase.trim(); // O método "trim" retorna o valor da string sem espaçamentos no inicio e no final
-
+const palavras = frase.trim(); // O método "trim" remove espaços no inicio e no final
 console.log(palavras);
 
 const frase2 = "JavaScript é incrível!";
+console.log(frase2.startsWith("Java")); // "startsWith" => Retorna "true" caso haja correspondência exata, mesmo que incompleta, no inicio da string
+console.log(frase2.startsWith("java")); // "startsWith" => Retorna "false" caso não haja correspondência exata, também no inicio da string
+console.log(frase2.endsWith("!")); // "endsWith" => Retorna "true" caso haja correspondência exata, porem, no final da string
+console.log(frase2.endsWith("?")); // "endsWith" => Retorna "false" caso não haja correspondência exata, também no final da string
 
-console.log(frase2.startsWith("Java")); // Retorna "true" caso haja correspondência exata, mesmo que incompleta, no inicio da string
-console.log(frase2.startsWith("java")); // Retorna "false" caso haja alguma não-correspondência, também no inicio da string
-
-console.log(frase2.endsWith("!")); // Retorna "true" caso haja correspondência exata, porem, no final da string
-console.log(frase2.endsWith("?")); // Retorna "false" caso haja alguma não-correspondência, também no final da string
-
-//Exceções e tratamento de erros
+// Exceções e tratamento de erros => Quando temos um erro, o JavaScript encerra por completo a execução do programa
 const idade = 15;
 
 // Executa um código em "try" e exibe erros em "catch"
 try {
   if (idade < 18) {
-    throw new Error("Voce deve ter pelo menos 18 anos!"); // Criando um objeto "Error" a partir de uma classe "new", onde "throw" executa esse erro, quando temos um erro, o JavaScript encerra por completo a execução do programa
+    throw new Error("Voce deve ter pelo menos 18 anos!"); // Criando um objeto "Error" a partir de uma classe "new", onde "throw" executa esse erro
   }
 } catch (error) {
   console.log(error.message); // O ".message" retorna apenas o conteúdo da mensagem
@@ -62,10 +65,11 @@ const mostrarMensagem = () => {
   console.log("Essa mensagem será exibida após 3 segundos.");
 };
 
-setTimeout(mostrarMensagem, 300000); // 1000ms = 1s
+setTimeout(mostrarMensagem, 10000); // 1000ms = 1s
+
 setTimeout(() => {
   console.log("Oi!"); // Não recebe uma função nomeada ou anônima, e sim, recebe uma callback
-}, 500000);
+}, 15000);
 
 // Promises => Promete que irá executar um código após uma condição
 const promessa = new Promise((resolve, reject) => {
@@ -80,17 +84,20 @@ const promessa = new Promise((resolve, reject) => {
 
 promessa
   .then((mensagem) => {
-    console.log(mensagem); // Executa a promessa => código prometido
+    console.log(mensagem); // ".then" => Executa o "resolve"
   })
   .catch((erro) => {
-    console.log(erro);
+    console.log(erro); // ".catch" => Executa o "reject"
   });
 
 // Bibliotecas "Promise based"
 const promise1 = Promise.resolve("Promessa instantânea"); // Uma "Promise" tem acesso direto ao método "resolve" e "Reject" sem passar por uma função
+console.log(promise1);
+
 const promise2 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 250000, "Promessa não instantânea");
+  setTimeout(resolve, 150000, "Promessa não instantânea");
 });
+console.log(promise2);
 
 Promise.all([promise1, promise2]).then((valores) => console.log(valores)); // Fazendo com que tanto a promessa com setTimeout, quanto a promessa instantânea sejam exibidas simultaneamente
 
@@ -101,7 +108,7 @@ const obterValor = async () => {
     setTimeout(() => resolve("Valor obtido"), 200000);
   });
 
-  const valor = await promessa;
+  const valor = await promessa; // "await" => Pausa a execução da função até que a promessa seja resolvida, após sua resolução, o valor obtido será armazenado na variável "valor"
   console.log(valor);
 };
 
@@ -111,11 +118,10 @@ obterValor();
 const obterValorComErro = async () => {
   try {
     const promessa = new Promise((resolve, reject) => {
-      setTimeout(() => reject("Valor com erro"), 300000);
+      setTimeout(() => reject("Valor com erro"), 1000);
     });
-
     const valorComErro = await promessa;
-    console.log(valorComErro);
+    console.log(valorComErro); // A tentativa de imprimir "valorComErro" será ignorada devido à rejeição da Promise, passando a execução para o bloco "catch"
   } catch (error) {
     console.log(error);
   }
@@ -123,16 +129,13 @@ const obterValorComErro = async () => {
 
 obterValorComErro();
 
-// JSON => JavaScript Object Notation
-// Padroniza a comunicação entre font-end e back-end em apenas uma linguagem
+// JSON(JavaScript Object Notation) => Padroniza a comunicação entre font-end e back-end em apenas uma linguagem
 const objeto = { nome: "João", idade: 21 };
-const jsonString = JSON.stringify(objeto); // Converte objeto para JSON
-
+const jsonString = JSON.stringify(objeto); // "stringify" => Converte objeto para JSON
 console.log(jsonString);
 console.log(typeof jsonString);
 
 const json = '{ "nome": "João", "idade": 21 }';
-const objeto2 = JSON.parse(json); // Converte JSON para objeto
-
+const objeto2 = JSON.parse(json); // "parse" => Converte JSON para objeto
 console.log(objeto2);
 console.log(typeof objeto2);
--- Fundamentos/Fundamentos-Basicos/Fund-basicos.js
+++ Fundamentos/Fundamentos-Basicos/Fund-basicos.js
@@ -1,42 +1,51 @@
 // var nomeDaVariável = "valorDaVariável"
 var nomeDaVariável = "Olá mundo!";
-console.log(nomeDaVariável);
+console.log(nomeDaVariável); // Imprime um valor no console
 
 // Variáveis e tipos de dados
+// Number
 var meuNumero = 10;
 console.log(meuNumero);
-console.log(meuNumero + 15);
+console.log(meuNumero + 15); // Concatenação
+console.log(typeof meuNumero); // "typeof" => Faz a checagem sobre qual tipo de dado esta armazenado
+console.log("10" + 5); // Uso de "" em valor numérico(number) é interpretado como texto(string)
 
-// Uso de "" em valor numérico é interpretado como texto
-console.log("10" + 5);
-
-// Faz a checagem sobre qual tipo de dado esta armazenado
-console.log(typeof meuNumero);
-
+// Boolean
 var booleano1 = true;
-var booleano2 = false;
-
 console.log(booleano1);
+
+var booleano2 = false;
 console.log(typeof booleano2);
 
-var meuObjeto = {}; // Objeto
-var meuArray = []; // Array
-var meuNull = null; // Null
-var meuUndefined = undefined; // Undefined
-
+// Objeto
+var meuObjeto = {};
 console.log(typeof meuObjeto);
+
+// Array
+var meuArray = [];
 console.log(typeof meuArray);
+
+// Null
+var meuNull = null;
 console.log(typeof meuNull);
+
+// Undefined
+var meuUndefined = undefined;
 console.log(typeof meuUndefined);
 
-// Novas formas de declarar variáveis => "var" e "const"
-let x = 10;
+// Tipagem fraca => Uma variável pode ter o seu tipo alterado ao logo do código
+var numero2 = 10; // "Number"
+console.log(typeof numero2);
 
+numero2 = "10" // String
+console.log(typeof numero2);
+
+// Novas formas de declarar variáveis => "let" e "const"
+let x = 10;
 console.log(typeof x);
 
-// Use "const" para garantir que não sejam alterados, valores que não devem ser alterados
+// Use "const" para garantir que os valores não serão alterados
 const y = 15;
-
 console.log(x, y);
 
 // Operadores aritméticos
@@ -51,22 +60,20 @@ console.log(x != y); // Verifica se valores são diferentes
 console.log(x === y); // Leva em consideração o tipo do dado analisado
 console.log(x !== y); // Verifica apenas se o tipo dos dados são diferentes
 
-// Operadores lógicos => "and = &&" "or = ||"
+// Operadores lógicos => "&& = and" "|| = or"
 console.log(10 > 5 && 5 > 10); // Verifica ambos os lados da operação
-console.log(10 > 5 || 5 > 10); // Verifica apenas um lado da operação
+console.log(10 > 5 || 5 > 10); // Caso o primeiro lado for verdadeiro, verifica apenas um lado da operação
 
 // Conversão de tipos de dados
-const meuNumero2 = "123";
-
-const meuNumeroConvertido = Number(meuNumero2);
-
+const meuNumero2 = "123"; // String
+const meuNumeroConvertido = Number(meuNumero2); // Number() => Convertido para "number"
 console.log(meuNumeroConvertido);
 console.log(typeof meuNumeroConvertido);
 
 // Estrutura de condição => "if", "else", "else if"
 const idade = 20;
 
-// Se a idade for menor que 13, será criança, se for menor que 20, adolescente e se for igual/maior a 20, será adulto
+// Se a idade for menor que 13, será criança, se for menor que 20, adolescente e se for igual/maior que 20, será adulto
 if (idade < 13) {
   console.log("Criança");
 } else if (idade < 20) {
@@ -75,9 +82,10 @@ if (idade < 13) {
   console.log("Adulto");
 }
 
-// Switch
+// Switch => Alternativa ao "if", "else" ideal para quando se tem varias opções de condição
 const fruta = "mamão";
 
+// Caso nenhuma das opções forem verdadeiras, sera executado o "default"
 switch (fruta) {
   case "banana":
     console.log("Banana é a fruta!");
@@ -89,7 +97,7 @@ switch (fruta) {
     console.log("Fruta não encontrada!");
 }
 
-// Estruturas de repetição => Repete varias vezes dependendo de alguma condição / Contador - Condição de limite - Incremento
+// Estruturas de repetição => Repete varias vezes dependendo de alguma condição => Contador - Condição de limite - Incremento
 // For
 for (let f = 0; f < 10; f++) {
   console.log("O valor de f é:" + f); // Concatenação = Unir variáveis ao meu texto
@@ -118,21 +126,20 @@ function cumprimentar(nome) {
 
 cumprimentar("João"); // Invocando a função
 
-// Arrow function
-const testeArrow = () => {
-  console.log("Isso também é uma função.");
-}
+// Arrow function => Forma de declarar função mais moderna
+const cumprimentar2 = (nome) => {
+  console.log("Olá! " + nome);
+};
 
-testeArrow();
+cumprimentar2("Pedro");
 
 // Escopo de variáveis
 let cor = "azul";
-
 console.log(cor); // Imprime o valor do escopo global
 
 const mostrarCor = () => {
   let cor = "Verde";
-  console.log(cor);
+  console.log(cor); // Imprime o valor do escopo local
 };
 
 mostrarCor();
@@ -163,39 +170,31 @@ if (minhaVariável2) {
 
 // Array / Listas => Caracterizado pela abertura e fechamento de colchetes "[]"
 const números = [1, 2, 3, 4, 5];
-
 console.log(números);
-console.log(números[0]); // O primeiro valor sempre inicia com zero
+console.log(números[0]); // Imprime o primeiro valor do array, que tem o índice "0"
 
-números.push(6); // Adicionando valores no array
-
+números.push(6); // Adicionando valores ao final no array
 console.log(números);
 
-números.pop();
+números.pop(); // Remove o ultimo valor do array
 console.log(números);
 
 // Strings
 const minhaStringNova = "Olá, Mundo! ";
-
 const minhaString2 = minhaStringNova + "Tudo bem?"; // Concatenação
-
 console.log(minhaString2);
 
 const minhaString3 = `${minhaStringNova}Fazendo interpolação`; // Interpolação => Concatenação mais moderna
-
 console.log(minhaString3);
-
-console.log(minhaString3.length); // Exibe a quantidade de caracteres
+console.log(minhaString3.length); // Exibe o comprimento (quantidade de caracteres) da string
 console.log(minhaString3[5]); // Exibe um carácter específico
 console.log(minhaString3.toUpperCase()); // Exibe a string em caixa alta
 
 // Objeto "Date"
-const agora = Date();
-
+const agora = new Date(); // Exibe a data completa atual
 console.log(agora);
 
 const natal = new Date(2023, 11, 25); // "Ano", "Mês (Iniciasse no zero)", "Dia"
-
 console.log(natal);
 
 // Objeto "Math"
