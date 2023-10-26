b8f68259cde741b23162eb5c79856188388337d3
João Bertholino <joaobertholino964@gmail.com>
2023-10-26 14:24:25 -0300

    Subindo inicio do curso e organização de pastas

---
+++ Curso-JS-YouTube/1 - Declarando-variaveis/JS/script.js
@@ -0,0 +1,26 @@
+// Declarando variáveis
+var meuTeste = 1; // Declarando variável de tipo "number" usando a sintaxe de "camelCase"
+console.log(meuTeste); // "console.log()" => Exibe um valor no console
+
+meuTeste = "João"; // Mudando o valor e o tipo da mesma variável para "string" (Tipagem fraca)
+console.log(meuTeste);
+
+meuNome = "André"; // Forma não adequada de declarar variável, podendo sobrescrever uma variável com o mesmo nome preexistente
+console.log(meuNome);
+
+var meuPrimeiroNome = "Victor"; // "var" => Possui escopo de função, sendo visíveis em toda a função que foram declaradas
+console.log(meuPrimeiroNome);
+
+// Declarando variáveis de forma moderna "let", "const"
+let testandoLet = "Declaração usando 'let'"; // "let" => Possui escopo de bloco, sendo visíveis apenas dentro do bloco que foram declaradas
+console.log(testandoLet);
+
+const testandoConst = "Declaração usando 'const'"; // "const" => Possui escopo de bloco, porém, não podendo ter ser valor alterado
+console.log(testandoConst);
+
+// Declarando variável sem valor => "undefined"
+let umUndefined;
+console.log(umUndefined);
+
+umUndefined = "Valor novo"; // Atribuindo um valor a variável sem valor 
+console.log(umUndefined);
---
+++ Curso-JS-YouTube/2 - Tipos-de-Dados/JS/script.js
@@ -0,0 +1,27 @@
+// Tipos de dados => "number", "string", "boolean", "null", "undefined", "object"
+
+// "number" => Tipo numérico
+let meuNumber = 10;
+console.log(typeof meuNumber); // "typeof" => Retorna o tipo do dado declarado
+
+// "string" => Tipo de texto
+let minhaString = "João";
+console.log(typeof minhaString);
+
+// "boolean" => Pode retornar dois valores "true" ou "False"
+let meuBoolean1 = true;
+console.log(typeof meuBoolean1);
+
+let meuBoolean2 = false;
+console.log(typeof meuBoolean2);
+
+// "null" => Inicialmente possui um valor nulo, podem ser definido mais tarde
+let meuNull = null;
+console.log(typeof meuNull);
+
+// "undefined" => Não possui valor definido
+let meuUndefined;
+console.log(typeof meuUndefined);
+
+// https://youtu.be/7Isq6ekQT1k?list=PLnDvRpP8BneysKU8KivhnrVaKpILD3gZ6&t=194
+// 3:14 - Tempo do video
