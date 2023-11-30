bae55c51e83735b811e6f32da5968fce5a847b37
João Bertholino <joaobertholino964@gmail.com>
2023-11-30 16:27:50 -0300

    Subindo exercícios

---
+++ JS - Exercicios basico ao avançado/1 - Declarando variaveis/JS/script.js
@@ -0,0 +1,25 @@
+// Exercício 1 => Declare duas variáveis "nome" e "idade" e atribua valores a elas, em seguida, imprima os valores no console
+const myName = "João";
+const myAge = 21;
+console.log(`Meu nome: ${myName}`);
+console.log(`Minha idade é: ${myAge}`);
+
+// Exercício 2 => Declare um constante chamada "pi", atribua o valor de PI e imprima no console
+const pi = Math.PI;
+console.log(`O valor de PI é: ${pi}`);
+
+// Exercício 3 => Declare um variável e atribua o maior numero que o JavaScript suporta e imprima no console
+const maxValue = Number.MAX_VALUE;
+console.log(`O maior numero suportado no JS é: ${maxValue}`);
+
+// Exercício 4 => Declare sue nome e sobrenome em duas variáveis, após isso declare um terceira variável que recebera a concatenação do valor das duas primeiras variáveis e imprima
+const firstName = "João";
+const lastName = "Bertholino";
+const fullName = `${firstName} ${lastName}`
+console.log(`Meu nome completo é: ${fullName}`);
+
+// Execício 5 => Declare duas variáveis de tipo "number" e utilize o operador de adição para realizar operações entre elas e imprima
+const fistNumber = 10;
+const lastNumber = 20;
+const addiction = fistNumber + lastNumber;
+console.log(`A soma dos valores da variáveis é: ${addiction}`);
