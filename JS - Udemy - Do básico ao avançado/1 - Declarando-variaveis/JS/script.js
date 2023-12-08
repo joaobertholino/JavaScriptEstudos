55f9b9bfa89dc2c95382198470bd2401fd9583c2
João Bertholino <joaobertholino964@gmail.com>
2023-12-08 14:32:33 -0300

    Subindo modulo 1

--- JS - Udemy - Do básico ao avançado/1 - Declarando-variaveis/JS/script.js
+++ JS - Udemy - Do básico ao avançado/1 - Declarando-variaveis/JS/script.js
@@ -1,11 +1,11 @@
 // Number => Tipo de números podem ser reais, inteiros etc
-let typeNumberOne = 10;
-console.log(typeof typeNumberOne); // typeof => Retorna o tipo do dado no console
+const typeNumberOne = 10;
+console.log(typeof typeNumberOne); // typeof => Retorna o tipo de dado no console
 
-let typeNumberTwo = 10.5;
+const typeNumberTwo = 10.5;
 console.log(typeof typeNumberTwo);
 
-let typeNumberThree = -10.5;
+const typeNumberThree = -10.5;
 console.log(typeof typeNumberThree);
 
 // Operações aritméticas e operadores => O retorno de valores sempre será do tipo number
@@ -15,39 +15,39 @@ console.log(typeof typeNumberThree);
 // 3° => Multiplicação, divisão e divisão por inteiro
 // 4° => Adição e subtração
 // + => Adição
-let addictionOne = 10;
-let addictionTwo = 15;
-let resultOne = addictionOne + addictionTwo; // Armazenando na variável "resultOne" a soma dos valores de "addictionOne" e "addictionTwo"
+const addictionOne = 10;
+const addictionTwo = 15;
+const resultOne = addictionOne + addictionTwo; // Armazenando na variável "resultOne" a soma dos valores de "addictionOne" e "addictionTwo"
 console.log(resultOne);
 
 // - => Subtração
-let subtractionOne = 15;
-let subtractionTwo = 10;
-let resultTwo = subtractionOne - subtractionTwo;
+const subtractionOne = 15;
+const subtractionTwo = 10;
+const resultTwo = subtractionOne - subtractionTwo;
 console.log(resultTwo);
 
 // * => Multiplicação
-let multiOne = 10;
-let multiTwo = 5;
-let resultThree = multiOne * multiTwo;
+const multiOne = 10;
+const multiTwo = 5;
+const resultThree = multiOne * multiTwo;
 console.log(resultThree);
 
 // / => Divisão
-let divideOne = 30;
-let divideTwo = 2;
-let resultFour = divideOne / divideTwo;
+const divideOne = 30;
+const divideTwo = 2;
+const resultFour = divideOne / divideTwo;
 console.log(resultFour);
 
 // % => Resto da divisão(Módulo)
-let restDivisionOne = 10;
-let restDivisionTwo = 3;
-let resultFive = restDivisionOne % restDivisionTwo;
+const restDivisionOne = 10;
+const restDivisionTwo = 3;
+const resultFive = restDivisionOne % restDivisionTwo;
 console.log(resultFive);
 
 // ** => Exponenciação
-let exponentOne = 10;
-let exponentTwo = 2;
-let resultSix = exponentOne ** exponentTwo;
+const exponentOne = 10;
+const exponentTwo = 2;
+const resultSix = exponentOne ** exponentTwo;
 console.log(resultSix);
 
 // Operadores incremental e decremental => Incrementam em +1 ou decrementam em -1 o valor da variável
@@ -56,7 +56,7 @@ let increment = 10;
 let resultSeven = ++increment; // Incrementa o valor da variável antes de retornar o valor
 console.log(resultSeven);
 
-let resultEight = increment++; // Incrementa o valor da variável depois de retornar o valor
+const resultEight = increment++; // Incrementa o valor da variável depois de retornar o valor
 console.log(resultEight);
 
 // -- => Decremento
@@ -64,99 +64,223 @@ let decrement = 10;
 let resultNine = --decrement;
 console.log(resultNine);
 
-let resultTen = decrement--;
+const resultTen = decrement--;
 console.log(resultTen);
 
 // Operadores aritméticos de atribuição => Executam a operação entre o valor da esquerda com o valor da direita e atribui esse resultado a variável da esquerda
 // += => Adição atribuída
 let assignedAdditionOne = 10;
-let assignedAdditionTwo = 10;
+const assignedAdditionTwo = 10;
 assignedAdditionOne += assignedAdditionTwo;
 console.log(assignedAdditionOne);
 
 // -= => Subtração atribuída
 let assignedSubtractionOne = 10;
-let assignedSubtractionTwo = 6;
+const assignedSubtractionTwo = 6;
 assignedSubtractionOne -= assignedSubtractionTwo;
 console.log(assignedSubtractionOne);
 
 // /= => Divisão atribuída
 let assignedDivisionOne = 10;
-let assignedDivisionTwo = 3;
+const assignedDivisionTwo = 3;
 assignedDivisionOne /= assignedDivisionTwo;
 console.log(assignedDivisionOne);
 
 // %= => Módulo atribuído
 let assignedModuleOne = 10;
-let assignedModuleTwo = 3;
-assignedModuleOne %= assignedModuleTwo;
+const assignedModuconstwo = 3;
+assignedModuleOne %= assignedModuconstwo;
 console.log(assignedModuleOne);
 
 // Special Numbers => São valores que não se comportam como números normais, sendo úteis para lidar com casos extremos e situações em que as operações matemáticas não podem ser realizadas
 // NaN => Not a Number => Representa um valor que não é um número válido
-let notNumber = 0 / 0;
+const notNumber = 0 / 0;
 console.log(notNumber);
 console.log(typeof notNumber);
 
 // Infinity => Infinito positivo => Representa o valor positivo infinito
-let infinity = 1 / 0;
+const infinity = 1 / 0;
 console.log(infinity);
 console.log(typeof infinity);
 
 // -Infinity => Infinito negativo => Representa o valor negativo infinito
-let infinityNegative = 10 - infinity;
+const infinityNegative = 10 - infinity;
 console.log(infinityNegative);
 console.log(typeof infinityNegative);
 
 // String => Tipo de texto podem ser escrito entre aspas duplas, simples ou utilizando as templates literals, tudo que for colocado entre aspas se tornara uma string
-let typeStringOne = `Victor`;
-let typeStringTwo = `João ${typeStringOne} Bertholino`; // Interpolação => Maneira moderna de se concatenar valores de outras variáveis na string.
+const typeStringOne = `Victor`;
+const typeStringTwo = `João ${typeStringOne} Bertholino`; // Interpolação => Maneira moderna de se concatenar valores de outras variáveis na string.
 console.log(typeStringTwo);
 console.log(typeof typeStringTwo);
 
 // Detalhes técnicos sobre o tipo string:
 // Escape sequences => As escape sequences são usadas para inserir caracteres especiais em uma string. Por exemplo, a sequência \n representa uma quebra de linha
-let escapeSequencesOne = `Quebrando a linha \ncom Escape sequences`; // \n => Quebra de linha
+const escapeSequencesOne = `Quebrando a linha \ncom Escape sequences`; // \n => Quebra de linha
 console.log(escapeSequencesOne);
 
-let escapeSequencesTwo = `Tabulação\tHorizontal`; // \t => Tabulação horizontal
+const escapeSequencesTwo = `Tabulação\tHorizontal`; // \t => Tabulação horizontal
 console.log(escapeSequencesTwo);
 
-let escapeSequencesThree = `Quebra de linha\r\ne retorno de carro`; // \r => Quebra de linha e retorno de carro
+const escapeSequencesThree = `Quebra de linha\r\ne retorno de carro`; // \r => Quebra de linha e retorno de carro
 console.log(escapeSequencesThree);
 
-let escapeSequencesFour = `Essa \'string\' possui aspas simples`; // \' => Aspas simples
+const escapeSequencesFour = `Essa \'string\' possui aspas simples`; // \' => Aspas simples
 console.log(escapeSequencesFour);
 
-let escapeSequencesFive = `Essa \"string\" possui aspas duplas`; // " => Aspas duplas
+const escapeSequencesFive = `Essa \"string\" possui aspas duplas`; // " => Aspas duplas
 console.log(escapeSequencesFive);
 
-let escapeSequencesSix = `Essa é uma \\string\\ com barras invertidas`; // \\ => Barra invertida
+const escapeSequencesSix = `Essa é uma \\string\\ com barras invertidas`; // \\ => Barra invertida
 console.log(escapeSequencesSix);
 
-let escapeSequencesSeven = `Essa é uma\0string\0com caractere nulo`; // \0 => Caractere nulo
+const escapeSequencesSeven = `Essa é uma\0string\0com caractere nulo`; // \0 => Caractere nulo
 console.log(escapeSequencesSeven);
 
-let escapeSequencesEight = `Essa é uma \x48 string \x48 com um caractere hexadecimal`; // \xNN => Caractere hexadecimal
+const escapeSequencesEight = `Essa é uma \x48 string \x48 com um caractere hexadecimal`; // \xNN => Caractere hexadecimal
 console.log(escapeSequencesEight);
 
-let escapeSequencesNine = `Essa é uma \u0048 string \u0048 com um caractere unicode`; // \uNNNN => Caractere Unicode
+const escapeSequencesNine = `Essa é uma \u0048 string \u0048 com um caractere unicode`; // \uNNNN => Caractere Unicode
 console.log(escapeSequencesNine);
 
 // Para inserir algo entre aspas em uma string é necessário inserir aspas inversas ao tipo utilizado na declaração da string
-let quotationMarks = "Como inserir 'aspas' em uma string!";
+const quotationMarks = "Como inserir 'aspas' em uma string!";
 console.log(quotationMarks);
 
 // Com template literals é possível realizar operações como:
-let tlNumber = 2;
-let tlOperation = `A soma de 2 + 2 é: ${tlNumber + tlNumber}`;
+const tlNumber = 2;
+const tlOperation = `A soma de 2 + 2 é: ${tlNumber + tlNumber}`;
 console.log(tlOperation);
 
 // Concatenar é um processo de incorporar strings:
-let concatOne = `Salada`;
-let concatTwo = `frutas`;
-let concatTrue = concatOne + " de " + concatTwo;
+const concatOne = `Salada`;
+const concatTwo = `frutas`;
+const concatTrue = concatOne + " de " + concatTwo;
 console.log(concatTrue);
 
-// Boolean => Tipo que retorna um "True" ou "False"
-// Empty Values => "null", "undefined"
+// Boolean => Retorna um "True" ou "False", utilizando operadores de comparação e lógicos
+// Operadores de comparação:
+// == => (Igualdade) => Verifica se dois valores são iguais, convertendo os tipos se necessário
+const equalityOne = 10;
+const equalityTwo = "10";
+const equalityVerified = equalityOne == equalityTwo; // Comparação sem levar em conta o tipo de dado comparado
+console.log(equalityVerified);
+
+// === => (Igualdade Estrita) => Verifica se dois valores são iguais, sem converter os tipos
+const strictEqualityOne = 10;
+const strictEqualityTwo = "10";
+const strictEqualityVerified = strictEqualityOne === strictEqualityTwo; // Comparação levando em conta o tipo de dado comparado
+console.log(strictEqualityVerified);
+
+// != => (Desigualdade) => Verifica se dois valores são diferentes, convertendo os tipos se necessário
+const inequalityOne = 10;
+const inequalityTwo = "20";
+const inequalityVerified = inequalityOne != inequalityTwo;
+console.log(inequalityVerified);
+
+// !== => (Desigualdade Estrita) => Verifica se dois valores são diferentes, sem converter os tipos
+const inequalityStrictOne = 10;
+const inequalityStrictTwo = 10;
+const inequalityStrictVerified = inequalityStrictOne !== inequalityStrictTwo;
+console.log(inequalityStrictVerified);
+
+// > => (Maior que) => Verifica se o valor da esquerda é maior que o valor da direita
+const biggerThenOne = 20;
+const biggerThenTwo = 10;
+const biggerThenVerified = biggerThenOne > biggerThenTwo;
+console.log(biggerThenVerified);
+
+// < => (Menor que) => Verifica se o valor da esquerda é menor que o valor da direita
+const lessThenOne = 20;
+const lessThenTwo = 10;
+const lessThenVerified = lessThenOne < lessThenTwo;
+console.log(lessThenVerified);
+
+// >= => (Maior ou igual a) => Verifica se o valor da esquerda é maior ou igual ao valor da direita
+const biggerEqualOne = 20;
+const biggerEqualTwo = 10;
+const biggerEqualVerified = biggerEqualOne >= biggerEqualTwo;
+console.log(biggerEqualVerified);
+
+// <= => (Menor ou igual a) => Verifica se o valor da esquerda é menor ou igual ao valor da direita
+const smallerEqualOne = 20;
+const smallerEqualTwo = 10;
+const smallerEqualVerified = smallerEqualOne <= smallerEqualTwo;
+console.log(smallerEqualVerified);
+
+// Operadores Lógicos:
+// && => (AND) => Retorna true se ambos os operandos forem true
+const andOperatorOne = 10;
+const andOperatorTwo = 20;
+const andOperatorVerified = andOperatorOne < andOperatorTwo && andOperatorTwo > andOperatorOne;
+console.log(andOperatorVerified);
+
+// || => (OR) => Retorna true se pelo menos um dos operandos for true
+const orOperatorOne = 10;
+const orOperatorTwo = 20;
+const orOperatorVerified = orOperatorOne > orOperatorTwo || orOperatorTwo < orOperatorOne;
+console.log(orOperatorVerified);
+
+// ! => (NOT) => Inverte o valor do operando, se for true retorna false e vice-versa
+const notOperatorOne = !false;
+console.log(notOperatorOne);
+
+const notOperatorTwo = !true;
+console.log(notOperatorTwo);
+
+// Operador Ternário => Executa uma comparação, semelhante ao que o ifElse faz, em apenas um linha de código
+// condição ? valorSeVerdadeiro : valorSeFalso
+const ternaryOperator = 10;
+const ternaryOperatorVerified = ternaryOperator % 2 === 0 ? true : false;
+console.log(ternaryOperatorVerified);
+
+// Empty Values
+// null => Representa a ausência intencional de qualquer objeto ou valor
+const emptyValueOne = null;
+console.log(emptyValueOne);
+
+// undefined => Representa uma variável que foi declarada, mas ainda não foi atribuída com um valor
+let emptyValueTwo;
+console.log(emptyValueTwo);
+
+// NaN => Representa um valor que não é um número válido
+const emptyValueThree = 0 / 0;
+console.log(emptyValueThree);
+console.log(typeof emptyValueThree);
+
+// Conversão de tipo automática => JavaScript é uma linguagem fracamente tipada, isso significa que o tipo de dado de uma variável pode ser alterado a qualquer momento
+// Conversão de string para number
+const conversionOne = "10";
+const conversionTwo = 10;
+const conversionThree = conversionOne * conversionTwo;
+console.log(conversionThree);
+
+// Conversão de number para string
+const conversionFour = 10;
+const conversionFive = "10";
+const conversionSix = conversionFour + conversionFive;
+console.log(conversionSix);
+
+// Conversão de boolean para number
+const conversionSeven = true;
+const conversionEight = 10;
+const conversionNine = conversionSeven + conversionEight;
+console.log(conversionNine);
+
+// Conversão de number para boolean
+const conversionTen = 10;
+const conversionEleven = true;
+const conversionTwelve = conversionTen + conversionEleven;
+console.log(conversionTwelve);
+
+// Conversão de string para boolean
+const conversionThirteen = "10";
+const conversionFourteen = true;
+const conversionFifteen = conversionThirteen + conversionFourteen;
+console.log(conversionFifteen);
+
+// Conversão de boolean para string
+const conversionSixteen = true;
+const conversionSeventeen = "10";
+const conversionEighteen = conversionSixteen + conversionSeventeen;
+console.log(conversionEighteen);
