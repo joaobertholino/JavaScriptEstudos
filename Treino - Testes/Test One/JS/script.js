d837430b9cd47a5e6454cd4a71f94d4d5f7c89d0
João Bertholino <joaobertholino964@gmail.com>
2024-01-15 15:43:29 -0300

    Subindo fibonacci

--- Treino - Testes/Test One/JS/script.js
+++ Treino - Testes/Test One/JS/script.js
@@ -1,17 +1,14 @@
 console.time('Tempo de execução');
 function fib(n) {
-  let x = 0;
-  let y = 1;
-  let temp;
+  let x = 0, y = 1, temp;
 
   for (let i = 0; i < BigInt(n); i++) {
     temp = BigInt(x) + BigInt(y);
-    x = y;
-    y = temp;
+    [x, y] = [y, temp];
   }
   console.log(temp);
 }
-fib(10000);
+fib(10);
 console.timeEnd('Tempo de execução');
 
 // console.time('Tempo de execução');
